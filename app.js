29d8e96484762cfa0dfebcb45ea8d162ca80bfff
ad1mohan <adi.mohan1999@gmail.com>
2022-06-28 15:27:13 +0530

    writeFileSync appendFileSync

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const fs = require("fs");
 
-fs.writeFileSync("log.txt", "This is my first log");
+fs.writeFileSync("log.txt", "My name is Aditya Mohan"); // Rewrites the existing file, creates one if not exists
+fs.appendFileSync("log.txt", "\nI am learning node.js"); // appends the existing file, creates one if not exists
