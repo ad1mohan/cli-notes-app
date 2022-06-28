5752f235ec127248ee5f6663c6aa7651b50c6f66
ad1mohan <adi.mohan1999@gmail.com>
2022-06-28 22:41:15 +0530

    added yargs

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
-const notes = require("./notes.js");
 const chalk = require("chalk");
+const { argv } = require("yargs");
+const yargs = require("yargs");
+/*const name = require("./log.js");
 
-const chalkLog = function () {
-	const log = console.log;
-	// Combine styled and normal strings
-	log(chalk.blue("Hello") + " World" + chalk.red("!"));
-	// Compose multiple styles using the chainable API
-	log(chalk.blue.bgRed.bold("Hello world!"));
-	// Pass in multiple arguments
-	log(chalk.blue("Hello", "World!", "Foo", "bar", "biz", "baz"));
-	// Nest styles
-	log(chalk.red("Hello", chalk.underline.bgBlue("world") + "!"));
-	// Nest styles of the same type even (color, underline, background)
-	log(
-		chalk.green(
-			"I am a green line " +
-				chalk.blue.underline.bold("with a blue substring") +
-				" that becomes green again!"
-		)
-	);
-	// ES2015 template literal
-	log(`
-        CPU: ${chalk.red("90%")}
-        RAM: ${chalk.green("40%")}
-        DISK: ${chalk.yellow("70%")}
-    `);
-	// Use RGB colors in terminal emulators that support it.
-	log(chalk.rgb(123, 45, 67).underline("Underlined reddish color"));
-	log(chalk.hex("#DEADED").bold("Bold gray!"));
-};
+require("./log.js");
+console.log("This is app js");
+console.log(name.lname);
+*/
 
-chalkLog();
+/*const valid = require("validator");
 
-// console.log(validator.isEmail("adi.mohan@mailwa.com"));
-// const validator = require("validator");
+console.log(valid.isEmail("adgdfs@afdf.com"));
+*/
+
+//const Chal = require("chalk");
+//console.log(chalk.green.bold("Success!"));
+//console.log(process.argv[2]);
+
+//console.log(yargs.argv);
+
+yargs.command({
+	command: "add",
+	describe: "Add a new note",
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			// type: "string",
+		},
+	},
+	handler: function (argv) {
+		console.log("Adding a new notes", argv);
+	},
+});
+
+// yargs.command({
+// 	command: "remove",
+// 	describe: "This will remove",
+// 	helper: function () {
+// 		console.log("this will remove file");
+// 	},
+// });
+
+// yargs.command({
+// 	command: "list",
+// 	describe: "This will describe the Note",
+// 	handler: function () {
+// 		console.log("This will describe Note");
+// 	},
+// });
+
+// yargs.command({
+// 	command: "read",
+// 	describe: "To read the Notes",
+// 	handler: function () {
+// 		console.log("Reading note");
+// 	},
+// });
+yargs.parse();
