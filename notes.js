0db8ff25f0d3333d08cd6ec3ba795d3afa4bea62
ad1mohan <adi.mohan1999@gmail.com>
2022-06-28 17:07:18 +0530

    npm modules importing

---
+++ notes.js
@@ -0,0 +1,7 @@
+const title = "This is title";
+const body = "This is body of the nodes";
+const getNotes = function () {
+	return "your notes";
+};
+
+module.exports = { title, body, getNotes };
--- utils.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const write = function () {
-	fs.writeFileSync("log.txt", "My name is Aditya Mohan Pandey"); // Rewrites the existing file, creates one if not exists
-	fs.appendFileSync("log.txt", "\nI am learning node.js"); // appends the existing file, creates one if not exists
-};
-
-module.exports = write;
